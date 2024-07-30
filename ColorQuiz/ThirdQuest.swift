b9e165d8db3895e7351e6457eb63afb66b811abb
Jordyn Chymbor-Souza <Jordyn@souza3.com>
2024-07-30 10:57:53 -0400

    Finish

---
+++ ColorQuiz/ThirdQuest.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdQuest.swift
+//  ColorQuiz
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct ThirdQuest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdQuest()
+}
---
+++ ColorQuiz/secondQuest.swift
@@ -0,0 +1,18 @@
+//
+//  secondQuest.swift
+//  ColorQuiz
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct secondQuest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    secondQuest()
+}
